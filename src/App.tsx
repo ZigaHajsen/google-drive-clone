bb97f0f386e6a60e0e7f9aebf3bc085c8a568ac1
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-01-28 16:46:50 +0100

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from './contexts/AuthContext';
-import { Signup } from './components/authentication';
+import { Signup, Dashboard } from './components/authentication';
 
 const App: React.FC = () => {
   return (
-    <AuthProvider>
-      <Container
-        className='d-flex align-items-center justify-content-center'
-        style={{ minHeight: '100vh' }}
-      >
-        <div className='w-100' style={{ maxWidth: '400px' }}>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+    <Container
+      className='d-flex align-items-center justify-content-center'
+      style={{ minHeight: '100vh' }}
+    >
+      <div className='w-100' style={{ maxWidth: '400px' }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route exact path='/' component={Dashboard} />
+              <Route path='/signup' component={Signup} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   );
 };
 
