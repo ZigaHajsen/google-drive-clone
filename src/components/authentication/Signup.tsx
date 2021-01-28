4e7128bf428c4de04f07e0e9c6be4f888ddae65a
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-01-28 16:54:35 +0100

    added Login link

--- src/components/authentication/Signup.tsx
+++ src/components/authentication/Signup.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from '../../contexts/AuthContext';
 
@@ -55,7 +56,7 @@ const Signup: React.FC = () => {
         </Card.Body>
       </Card>
       <div className='w-100 text-center mt-2'>
-        Already have an account? Log In
+        Already have an account? <Link to='/login'>Log In</Link>
       </div>
     </Fragment>
   );
