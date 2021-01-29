112ec573704cba4e31a91dd8dd09822f90defdb7
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-01-29 22:49:37 +0100

    added Dashboard component

--- src/components/authentication/Dashboard.tsx
+++ src/components/authentication/Dashboard.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Card, Button, Alert } from 'react-bootstrap';
+import { useAuth } from '../../contexts/AuthContext';
 
 const Dashboard: React.FC = () => {
-  return <div>Dashboard</div>;
+  const [error, setError] = useState('');
+  // @ts-ignore
+  const { currentUser } = useAuth();
+
+  const handleLogout = () => {};
+
+  return (
+    <Fragment>
+      <Card>
+        <Card.Body>
+          <h2 className='text-center mb-4'>Profile</h2>
+          {error && <Alert variant='danger'>{error}</Alert>}
+          <strong>Email:</strong> {currentUser.email}
+          <Link to='/update-profile' className='btn btn-primary w-100 mt-3'>
+            Update Profile
+          </Link>
+        </Card.Body>
+      </Card>
+      <div className='w-100 text-center mt-2'>
+        <Button variant='link' onClick={handleLogout}>
+          Log Out
+        </Button>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Dashboard;
