070faeb39fb9fe330cd035fc76cfbffd2764405d
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-01-30 19:34:13 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
 import { AuthProvider } from './contexts/AuthContext';
 import {
   Profile,
